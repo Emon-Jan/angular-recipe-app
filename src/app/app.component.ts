4af3d2d774dc78f538286e00997d384ef0e2edc6
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-04-26 16:31:27 +0600

    firebase user authentication added and initialized on starting app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   feature = 'recipe';
 
+  constructor() { }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDyEAjN7W1BQoHZjZGW8c326XzBoXh1uQA',
+      authDomain: 'recipe-book-b1add.firebaseapp.com'
+    });
+  }
   onNavigate(feature) {
     this.feature = feature;
   }
