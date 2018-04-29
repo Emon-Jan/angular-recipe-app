0ca80ad6f8df8021191922c4a03f2017ddf1939b
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-04-29 13:09:45 +0600

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,15 +11,6 @@ import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    {
-        path: 'recipes', component: RecipesComponent, children:
-            [
-                { path: '', component: RecipeStartComponent },
-                { path: 'new', component: RecipeEditComponent },
-                { path: ':id', component: RecipeDetailComponent },
-                { path: ':id/edit', component: RecipeEditComponent }
-            ]
-    },
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'signin', component: SigninComponent }
